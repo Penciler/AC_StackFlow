73970eaab485e5cec148edf5671acf5d2254121f
sinash <pine0113@gmail.com>
2018-06-20 00:28:32 +0800

    fix migration error

--- db/migrate/20180619122126_add_question_attribute.rb
+++
@@ -1,7 +0,0 @@
-class AddQuestionAttribute < ActiveRecord::Migration[5.1]
-  def change
-    add_column :questions, :user_id, :string
-    add_column :questions, :subject, :string
-    add_column :questions, :content, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180619123637) do
 
   create_table "answer_upvotes", force: :cascade do |t|
