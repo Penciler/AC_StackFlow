a872fb596ee39aec56fe85918d1162ecaa2ab2fb
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-07-05 21:29:24 +0800

    answer create fix

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,6 @@ class FavoritesController < ApplicationController
   def index
     @favorite_questions = current_user.favorites_questions
     @user = current_user
-    #@users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
   end
 
 end
